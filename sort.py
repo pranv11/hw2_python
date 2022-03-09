4f5909c0ee181325e058b3ca29530138cc7e38d1
pranv11 <87796245+pranv11@users.noreply.github.com>
2022-03-08 21:02:42 -0800

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,19 +1,24 @@
+from sqlalchemy import null
+
+
 def sort_list(list1):
     n = len(list1)
     i = 0
+    if n == 0:
+        return null
+    else:
+        while i < n :
+            if type(list1[i]) == int:
+                x = 0
 
-    while i < n :
-        if type(list1[i]) == int:
-            x = 0
-
-            while x < n - i - 1 :
-                if list1[x] > list1[x+1]:
-                    tmp = list1[x]
-                    list1[x] = list1[x+1]
-                    list1[x+1] = tmp
-                x = x+1
-            i += 1
-        return list1
+                while x < n - i - 1 :
+                    if list1[x] > list1[x+1]:
+                        tmp = list1[x]
+                        list1[x] = list1[x+1]
+                        list1[x+1] = tmp
+                    x = x+1
+                i += 1
+            return list1
 
 
 
@@ -21,12 +26,11 @@ def printL(list1):
     for i in range(len(list1)):
         print(list1[i])
 
-try:
-    abc = [1,8, 4, 3, 9 ,57]
+
+    abc = []
 
     printL(sort_list(abc))
-except:
-    print('the input is incorrect')
+
 
 
 
