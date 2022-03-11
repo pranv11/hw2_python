3aa695615c7dba2660f4d9a71104687bc4375a85
pranv11 <87796245+pranv11@users.noreply.github.com>
2022-03-10 19:50:53 -0800

    Add files via upload

--- timeit.py
+++ timeit.py
@@ -1,11 +1,18 @@
-import time
+import time as time
 
-def calculate_time(fnc):
+def calculate_time(func):
     def wrapper():
-        time_initial = time.time()
-        fnc()
-        time_final = time()
-
+        time_initial =time.time()
+        func()
+        time_final = time.time()
         print("Total time " + str(time_final-time_initial))
+        
+        
     return wrapper
 
+@calculate_time
+def printe():
+    print("hey")
+
+
+printe()
